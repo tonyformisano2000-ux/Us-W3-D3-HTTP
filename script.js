02f379cc30ee587d654f2f061d846466db9caaf6
Antonio <tony.formisano.2000@gmail.com>
2026-04-15 19:00:18 +0200

    a

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let carrello = [];
 const library = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => {
@@ -11,21 +12,34 @@ const library = () => {
     .then((response) => {
       console.log(response);
       const row = document.getElementById("row");
+      let item = 0;
       response.forEach((element, i) => {
         const card = document.createElement("div");
-        card.classList.add("card", "col-3", "h-5", "m-3");
-        card.innerHTML = `<img src=${element.img} class="card-img-top" alt="Copertina del libro">
-  <div class="card-body">
-    <h5 class="card-title">${element.title}</h5>
-    <p class="card-text">${element.price}</p>
-    <a onclick="remove(event)" class="btn btn-primary">Remove</a>
+        card.classList.add("card", "col-3", "h-6", "m-3", `itemN${item}`);
+        card.innerHTML = `<img src=${element.img} class="card-img-top " alt="Copertina del libro">
+  <div class="card-body d-flex flex-wrap align-items-center">
+    <h5 class="card-title text-center">${element.title}</h5>
+    <div class="d-flex justify-content-between w-100 flex-wrap">
+    <p class="card-text">${element.price}€</p>
+    <a onclick="remove(event)" class="btn btn-danger">Remove</a>
+     <a onclick="nelCarrello(event)" class="btn btn-primary mt-2 w-100">Add to chart</a> </div>
   </div>`;
         row.appendChild(card);
+        item += 1;
       });
     })
     .catch(console.log("errore: il server non è raggiungibile."));
 };
 library();
+
+let conteggioString = document.querySelector("header p");
+const nelCarrello = (e) => {
+  const card = e.target.closest(".card").className;
+  let conteggioParse = parseInt(conteggioString.innerText);
+  conteggioParse += 1;
+  conteggioString.innerText = conteggioParse;
+  console.log(conteggioString);
+};
 
 const remove = (e) => {
   console.log("hai cliccato su ", e.target);
